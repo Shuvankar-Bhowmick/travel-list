e4e0755f29aa26594b92ba80b9db1eaacc779fad
Shuvankar Bhowmick <bhowmickshuvankar@gmail.com>
2023-11-23 22:36:39 +0530

    Added code for the layout of the project.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
     </div>
   );
 }
 
-export default App;
+function Logo() {
+  return <h1>💼 Far Away 🌴</h1>;
+}
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😍 trip?</h3>
+    </div>
+  );
+}
+function PackingList() {
+  return <div className="list">LIST</div>;
+}
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>You have X items on your list, and you already packed X (X%)</em>
+    </footer>
+  );
+}
