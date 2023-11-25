9a12370e84e33ff76ff16fc4be425634e58c6c0e
Shuvankar Bhowmick <bhowmickshuvankar@gmail.com>
2023-11-26 01:28:47 +0530

    Code changes to make the application stateful

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./index.css";
+import { useState } from "react";
 
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: true },
@@ -21,14 +22,29 @@ function Logo() {
   return <h1>💼 Far Away 🌴</h1>;
 }
 function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
   function handleSubmit(e) {
     e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+
+    // Set the form to the initial state
+    setDescription("");
+    setQuantity(1);
   }
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-      <select>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => {
           return (
             <option value={num} key={num}>
@@ -37,7 +53,12 @@ function Form() {
           );
         })}
       </select>
-      <input type="text" placeholder="Item..."></input>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      ></input>
       <button>Add</button>
     </form>
   );
