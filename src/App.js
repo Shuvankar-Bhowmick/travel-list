c350a975da1c2df900ddffc89e38f9ff7458a692
Shuvankar Bhowmick <bhowmickshuvankar@gmail.com>
2023-11-26 22:34:10 +0530

    Code changes to render the PackingList component.

--- src/App.js
+++ src/App.js
@@ -8,11 +8,18 @@ const initialItems = [
 ];
 
 export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+    console.log(item);
+  }
+
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItems={handleAddItems} />
+      <PackingList items={items} />
       <Stats />
     </div>
   );
@@ -21,7 +28,7 @@ export default function App() {
 function Logo() {
   return <h1>💼 Far Away 🌴</h1>;
 }
-function Form() {
+function Form({ onAddItems }) {
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(1);
 
@@ -31,7 +38,9 @@ function Form() {
     if (!description) return;
 
     const newItem = { description, quantity, packed: false, id: Date.now() };
-    console.log(newItem);
+    // console.log(newItem);
+
+    onAddItems(newItem);
 
     // Set the form to the initial state
     setDescription("");
@@ -63,11 +72,12 @@ function Form() {
     </form>
   );
 }
-function PackingList() {
+
+function PackingList({ items }) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
+        {items.map((item) => (
           <Item item={item} key={item.id} />
         ))}
       </ul>
