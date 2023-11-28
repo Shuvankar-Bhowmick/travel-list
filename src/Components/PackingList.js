10d080b9fa9896567d62cb38aab6ca1ddcb7d106
Shuvankar Bhowmick <bhowmickshuvankar@gmail.com>
2023-11-28 19:52:18 +0530

    Code changes to add sorting and list clearing functionalities.

---
+++ src/Components/PackingList.js
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import Item from "./Item";
+
+export default function PackingList({
+  items,
+  onDeleteItem,
+  onToggleItem,
+  onClearList,
+}) {
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItems;
+
+  if (sortBy === "input") sortedItems = items;
+  else if (sortBy === "description")
+    sortedItems = items
+      .slice()
+      .sort((x, y) => x.description.localeCompare(y.description));
+  else sortedItems = items.slice().sort((x, y) => Number(x.packed - y.packed));
+
+  return (
+    <div className="list">
+      <ul>
+        {sortedItems.map((item) => (
+          <Item
+            item={item}
+            key={item.id}
+            onDeleteItem={onDeleteItem}
+            onToggleItem={onToggleItem}
+          />
+        ))}
+      </ul>
+
+      <div className="actions">
+        <select
+          value={sortBy}
+          onChange={(e) => {
+            console.log(e.target.value);
+            setSortBy(e.target.value);
+          }}
+        >
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed"> Sort by packed status</option>
+        </select>
+        <button onClick={onClearList}>Clear list</button>
+      </div>
+    </div>
+  );
+}
